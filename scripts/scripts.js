59917fdab715a8d13de78f74aa478ab59baf50ca
deck-adobe <deckreyes@adobe.com>
2024-03-06 15:11:52 -0800

    Reverted back.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,36 +1,3 @@
-const AUDIENCES = {
-  mobile: () => window.innerWidth < 600,
-  desktop: () => window.innerWidth >= 600,
-  // define your custom audiences here as needed
-};
-
-/**
- * Gets all the metadata elements that are in the given scope.
- * @param {String} scope The scope/prefix for the metadata
- * @returns an array of HTMLElement nodes that match the given scope
- */
-export function getAllMetadata(scope) {
-  return [...document.head.querySelectorAll(`meta[property^="${scope}:"],meta[name^="${scope}-"]`)]
-    .reduce((res, meta) => {
-      const id = toClassName(meta.name
-        ? meta.name.substring(scope.length + 1)
-        : meta.getAttribute('property').split(':')[1]);
-      res[id] = meta.getAttribute('content');
-      return res;
-    }, {});
-}
-
-// Define an execution context
-const pluginContext = {
-  getAllMetadata,
-  getMetadata,
-  loadCSS,
-  //loadScript,
-  sampleRUM,
-  //toCamelCase,
-  toClassName,
-};
-
 import {
   sampleRUM,
   buildBlock,
@@ -216,14 +183,6 @@ async function loadEager(doc) {
     await runExperiment(experiment, instantExperiment, EXPERIMENTATION_CONFIG);
   }
 
-  if (getMetadata('experiment')
-  || Object.keys(getAllMetadata('campaign')).length
-  || Object.keys(getAllMetadata('audience')).length) {
-  // eslint-disable-next-line import/no-relative-packages
-  const { loadEager: runEager } = await import('../plugins/experimentation/src/index.js');
-  await runEager(document, { audiences: AUDIENCES }, pluginContext);
-}
-
   // load demo config
   await loadDemoConfig();
 
@@ -304,14 +263,6 @@ async function loadLazy(doc) {
   // eslint-disable-next-line import/no-relative-packages
   const { initConversionTracking } = await import('../plugins/rum-conversion/src/index.js');
   await initConversionTracking.call(context, document);
-
-  if ((getMetadata('experiment')
-    || Object.keys(getAllMetadata('campaign')).length
-    || Object.keys(getAllMetadata('audience')).length)) {
-    // eslint-disable-next-line import/no-relative-packages
-    const { loadLazy: runLazy } = await import('../plugins/experimentation/src/index.js');
-    await runLazy(document, { audiences: AUDIENCES }, pluginContext);
-  }
 }
 
 /**
